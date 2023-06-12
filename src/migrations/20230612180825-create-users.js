51bf46ebae9c8800213010a3fc305d35cb29929f
Felipe Mendes <felipemadruga@gmail.com>
2023-06-12 15:14:24 -0300

    Cria migration users

---
+++ src/migrations/20230612180825-create-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        primarykey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+   return queryInterface.dropTable('users');
+  },
+};
