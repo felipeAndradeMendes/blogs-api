cfdb15482ed35063057f31f857277ebf8dc47578
Felipe Mendes <felipemadruga@gmail.com>
2023-06-12 16:30:41 -0300

    Corrige erros em migrations e cria Model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: {
+        model: 'blog_posts',
+        key: 'id',
+      },
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: {
+        model: 'categories',
+        key: 'id',
+      },
+    },
+  },
+  {
+    tableName: 'posts_categories',
+    underscored: true,
+    timestamps: false,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      through: PostCategory,
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      through: PostCategory,
+    });
+  };
+
+  return PostCategory;
+};
