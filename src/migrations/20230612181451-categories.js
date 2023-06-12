feb127fc4c8450a34f31e2bce9f65cc1a6abcd7e
Felipe Mendes <felipemadruga@gmail.com>
2023-06-12 15:17:36 -0300

    Cria migration categories

---
+++ src/migrations/20230612181451-categories.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('categories', {
+      id: {
+        primarykey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('categories');
+  }
+};
